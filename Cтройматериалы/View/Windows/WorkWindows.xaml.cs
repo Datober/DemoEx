5d794e7d80f27401438b458cc844012a56e695e9
Damirstudent <ggarikov5@gmail.com>
2023-03-30 16:52:39 +0300

    Ex

---
+++ Cтройматериалы/View/Windows/WorkWindows.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cтройматериалы.View
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkWindows.xaml
+    /// </summary>
+    public partial class WorkWindows : Window
+    {
+        public WorkWindows()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddData_Click(object sender, RoutedEventArgs e)
+        {
+            //Кнопка позволяет добавить данные из БД
+        }
+    }
+    //Данное окно считывает информацию из БД, позволяя пользователю выбрать, удалить товар и редактировать его
+}
---
+++ Cтройматериалы/ViewModel/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cтройматериалы.ViewModel
+{
+   // public class BaseViewModel : IPInterfacePropertiesChanged { 
+          //  public event PropertyChangedEventHandler Property
+
+    }
+   // {
+   // protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+   //     PropertyChandge?.Invoke(this.new OnPropertyChanged)
+   // }
+//}
